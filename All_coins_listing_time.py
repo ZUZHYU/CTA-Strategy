dfd9a388c86383edcbdc9ed50a14da9f020b3bd7
ZUZHYU <93599847+ZUZHYU@users.noreply.github.com>
2026-02-24 17:53:26 +0100

    Add files via upload

---
+++ All_coins_listing_time.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datetime import datetime
+import pickle
+from glob import glob
+
+file = '/local/scratch/yuzhang_utxo/token_price'
+files = glob(file+'/*_minute_price.csv')
+print(len(files))
+
+token_list_time = dict()
+
+for f in files:
+    print(f)
+    symbol = f.split("/")[-1].split("_")[0]
+    tp = pd.read_csv(f,header=0)
+    timestamp = int(tp.loc[0][1]/1000)
+    dt = datetime.fromtimestamp(timestamp)
+    token_list_time[symbol]=dt
+
+with open(file+'/all_tokens_listing_time.csv') as f:
+    pickle.dump(token_list_time,f)
+
+print('finised')
